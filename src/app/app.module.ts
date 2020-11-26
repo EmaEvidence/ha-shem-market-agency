6f7b70ae82ee8cf79007049c27f43b7389f79d31
Emmanuel Alabi <emmanuelalabi@Emmanuels-MacBook-Pro.local>
2020-11-26 18:40:13 +0100

    implement ui components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+import { BannerComponent } from './banner/banner.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ServicesComponent } from './services/services.component';
+import { PricingplansComponent } from './pricingplans/pricingplans.component';
+import { TestimoniesComponent } from './testimonies/testimonies.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrandsComponent } from './brands/brands.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogoComponent,
+    NavComponent,
+    BannerComponent,
+    TestimonialsComponent,
+    ServicesComponent,
+    PricingplansComponent,
+    TestimoniesComponent,
+    FooterComponent,
+    BrandsComponent
   ],
   imports: [
     BrowserModule,
